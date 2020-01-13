0a9f30eae497d4c454902c8322d3fc4301ffe886
Pascal Belouin <pbelouin@mpiwg-berlin.mpg.de>
2020-01-13 12:22:49 +0100

    tweak script to pull using for loops

--- FetchTextFromRISE.py
+++ FetchTextFromRISE.py
@@ -1,36 +1,31 @@
 import requests
+import os
 
 # 0dbc904a-ffe9-41c4-866c-7a66434972c1 is the uuid for 哈佛燕京 collection
 # 750 is a random number for avoiding the limitation of pagination
-resources = requests.get("https://rise.mpiwg-berlin.mpg.de/api/collections/0dbc904a-ffe9-41c4-866c-7a66434972c1/resources?per_page=750").json()
+resources = requests.get("https://rise.mpiwg-berlin.mpg.de/api/collections/d8008b85-c4f1-47c8-9f4e-3c02c510307b/resources?per_page=6000")
 
-uuids = [ r['uuid'] for r in resources ]
+corpus_path = './corpus'
+if not os.path.exists(corpus_path):
+      os.makedirs(corpus_path)
 
-# for bookId in uuids[:1]:   # for test only, observe first book.
-for bookId in uuids:
-      #fetch metadata of the book based on book id. metadata is not be collected well yet.
-#     meta = requests.get("https://rise.mpiwg-berlin.mpg.de/api/resources/" + bookId + "/metadata").json()
-#     print(meta)
+for resource in resources.json():
+      uuid = resource['uuid']
+      resource_name = resource['name']
+      print(resource_name)
 
-    #fetch sections of the book based on book id.
-    #only one section for each book of HYL collection right now, therefore hardcode first element of reponse result.
-    section = requests.get("https://rise.mpiwg-berlin.mpg.de/api/resources/"+ bookId +"/sections").json()[0]
-
-    #for avoiding pagination mechanism, get total pages first
-    contentUnitCount = section['contentUnitCount']
-
-    #get section id
-    sectionId = section['uuid']
-
-    #fetch text from RISE
-    contents = requests.get("https://rise.mpiwg-berlin.mpg.de/api/sections/"+ sectionId +"/content_units?per_page=" + str(contentUnitCount) ).json()
-
-    #put all text into a list
-    texts = [c['content'] for c in contents]
-
-    # save into a file.
-    # the texts contain some special tags (such as <char> <tb> <ls> <pb>) that you need to deal with yourself.
-    with open("/Users/calvinyeh/Workspaces/local-gazetteers/lang-model/all_text_ori/" + bookId + ".txt", "w+") as file:
-             file.write('\n'.join(texts))
-
-print('Done')
+      if not os.path.exists(corpus_path + "/" + resource_name):
+            os.makedirs(corpus_path + "/" + resource_name)
+      sections = requests.get("https://rise.mpiwg-berlin.mpg.de/api/resources/"+ resource['uuid'] +"/sections")
+      
+      for section in sections.json():
+            print(section)
+            print(section['uuid'])
+            section_name = section['name']
+            section_path = corpus_path + "/" + resource_name + "/" + section_name
+            file = open(section_path +".txt", "w") 
+            content_units = requests.get("https://rise.mpiwg-berlin.mpg.de/api/sections/"+ section['uuid'] +"/content_units?per_page=6000")
+            for content_unit in content_units.json():
+                  print(content_unit)
+                  file.write(content_unit['content']) 
+            file.close()
