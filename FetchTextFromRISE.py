7c221f5f99d5efca962d86ee9f568361a8295247
pbelouin <pbelouin@mpiwg-berlin.mpg.de>
2020-08-03 13:00:20 +0200

    update script to allow for easier fetching of any publicly accessible collection

--- FetchTextFromRISE.py
+++ FetchTextFromRISE.py
@@ -1,15 +1,38 @@
+# -*- coding: utf-8 -*-
 import requests
 import os
 
 # 0dbc904a-ffe9-41c4-866c-7a66434972c1 is the uuid for 哈佛燕京 collection
 # 750 is a random number for avoiding the limitation of pagination
-resources = requests.get("https://rise.mpiwg-berlin.mpg.de/api/collections/d8008b85-c4f1-47c8-9f4e-3c02c510307b/resources?per_page=6000")
+per_page = 6000
+
+# getting the list of collections that the user has access to:
+collections_response = requests.get(f'https://rise.mpiwg-berlin.mpg.de/api/collections?per_page={per_page}')
+
+collections = collections_response.json()
+# each accessible collections has a name, a uuid, and a number of resources.
+# print(collections)
+idx = 1
+for collection in collections:
+      print(f'collection at index: {idx}')
+      idx += 1
+      print(collection)
+
+# we pick a collection by its index
+collection_index = 1
+collection = collections[collection_index]
+
+print(collection['uuid'])
+collection_uuid = collection['uuid']
+
+# we grab all resources for this collection
+resources_response = requests.get(f'https://rise.mpiwg-berlin.mpg.de/api/collections/{collection_uuid}/resources?per_page={per_page}')
 
 corpus_path = './corpus'
 if not os.path.exists(corpus_path):
       os.makedirs(corpus_path)
 
-for resource in resources.json():
+for resource in resources_response.json():
       uuid = resource['uuid']
       resource_name = resource['name']
       print(resource_name)
